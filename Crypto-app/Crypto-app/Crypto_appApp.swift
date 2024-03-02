d6771acb8509d7142a75ffcff25cc9b18f8f7616
Sherif Hamdy <93449644+sherifHamdyAhmed93@users.noreply.github.com>
2024-03-02 18:39:30 +0200

    init project

---
+++ Crypto-app/Crypto-app/Crypto_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_appApp.swift
+//  Crypto-app
+//
+//  Created by Sherif Hamdy on 02/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
